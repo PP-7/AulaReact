cd683822fb2444b90174942e32cfd4c5985595b9
PP-7 <pepepp019@icloud.com>
2025-04-08 12:20:56 -0300

    8/4/25

---
+++ boasvindas/src/App.jsx
@@ -0,0 +1,25 @@
+  import BoasVindas from "./Exemplo1"
+  
+  function App() {
+  
+  return (
+   <div>
+    <h1>Boas Vindas ao React com JavaScript</h1>
+
+    <p>Este é o arquivo App.jsx que será o noso ponto de partida para o desenvolvimento
+      de aplicação React utilizando JavaScript.
+    </p>
+
+    <BoasVindas nome="DoisP"/>
+    <BoasVindas nome="2P"/>
+    <BoasVindas nome="PP"/>
+    <BoasVindas nome="Pepe"/>
+    <BoasVindas nome="João Pedro"/>
+
+
+
+   </div>
+  )
+}
+
+export default App
