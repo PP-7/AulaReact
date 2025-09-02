27439c3beeb5d63c58e2206dd43faacade0370c6
PP-7 <pepepp019@icloud.com>
2025-09-02 12:10:47 -0300

    02/09/2025

---
+++ estudoestados/src/Paginas/Exemplo1.jsx
@@ -0,0 +1,30 @@
+export default function Exemplo1() {
+  return (
+    <div className="conteudo">
+      <h1>Exemplo 1</h1>
+
+      <div>
+        <h3>Exemplo de uso do UseState</h3>
+
+        <p>O objetivo será somar dois números e exibir o resultado na tela.</p>
+
+        <form>
+
+          <p> 
+            Digite o primeiro número<br/>
+            <input type="text"/>
+          </p>
+
+          <p>
+            Digite o segundo número<br/>
+            <input type="text"/>
+          </p>
+
+          <p>
+            <input type="button" value="Calcular"/>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+}
---
+++ estudoestados/src/Paginas/Exemplo2.jsx
@@ -0,0 +1,33 @@
+export default function Exemplo2() {
+  return (
+    <div className="conteudo">
+
+      <h1>Exemplo 2</h1>
+
+        <div>
+
+          <h3>Calculo de média</h3>
+
+            <p>
+              O objeivo será receber duas notas, calcular a média e exibir o resultado na tela.
+            </p>
+            
+            <p>
+              Digite o primerio número<br/>
+              <input type="text"/>
+            </p>
+
+            <p>
+              Digite o segundo número<br/>
+              <input type="text"/>
+            </p>
+
+            <p>
+            <input type="button" value="Calcular média"/>
+            </p>
+
+        </div>
+      
+    </div>
+  );
+}
